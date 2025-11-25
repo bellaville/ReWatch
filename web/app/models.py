4077e86c1f384c6e344f16a3cc0d2856a0768519
Bella Villeneuve <anabellavilleneuve@cmail.carleton.ca>
2025-11-24 20:31:22 -0500

    added role table and code to insert basic roles into the database on app startup

--- web/app/models.py
+++ web/app/models.py
@@ -1,9 +1,23 @@
 from flask_login import UserMixin
 from . import db
 
+# model for user objects
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+# model for roles
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+# association table for users and roles (many to many)
+roles_users = db.Table('roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
 
--- web/run.py
+++ web/run.py
@@ -1,10 +1,20 @@
 from app import create_app, db
+from app.models import Role
 
 app = create_app()
 
-# Optional: create tables if not exist
+# create tables, insert roles into database
 with app.app_context():
+    db.drop_all()
     db.create_all()
+    patient = Role(id=1, name='Patient')
+    physician = Role(id=2, name='Physician')
+
+    db.session.add(patient)
+    db.session.add(physician)
+
+    db.session.commit()
+    print("Roles created successfully!")
 
 if __name__ == "__main__":
     app.run(debug=True)
