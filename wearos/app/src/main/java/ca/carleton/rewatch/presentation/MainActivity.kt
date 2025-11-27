79c753bfe80ec9c7bc1a480f5a34fd6315b1a8f1
Nik Legault <nik.legault.12@gmail.com>
2025-11-26 20:17:26 -0500

    Started accessing the accelerometer through the android.hardware package instead of through SDK

--- wearos/app/src/main/java/ca/carleton/rewatch/presentation/MainActivity.kt
+++ wearos/app/src/main/java/ca/carleton/rewatch/presentation/MainActivity.kt
@@ -1,7 +1,10 @@
 package ca.carleton.rewatch.presentation
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
 import android.health.connect.HealthConnectException
 import android.os.Bundle
 import android.widget.TextView
@@ -15,6 +18,11 @@ import ca.carleton.rewatch.R
 import com.samsung.android.service.health.tracking.*
 import com.samsung.android.service.health.tracking.data.*
 
+import android.hardware.SensorManager
+import android.hardware.SensorEventListener
+import kotlin.math.abs
+
+
 class MainActivity : ComponentActivity() {
     private var healthTrackingService : HealthTrackingService? = null
     private var accelTracker : HealthTracker? = null
@@ -25,11 +33,54 @@ class MainActivity : ComponentActivity() {
     private lateinit var accelZ : TextView
     private lateinit var toggle : ToggleButton
 
+    private lateinit var sensorManager : SensorManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        status = findViewById(R.id.statusText)
+        accelX = findViewById(R.id.accel_x)
+        accelY = findViewById(R.id.accel_y)
+        accelZ = findViewById(R.id.accel_z)
+        toggle = findViewById(R.id.toggle_button)
+        toggle.isEnabled = true
+
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        var sensor : Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+
+        status.text = "Sensor loaded"
+    }
+
+    private val listener = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        }
+
+        override fun onSensorChanged(event: SensorEvent?) {
+            var x : Float = event!!.values[0]
+            var y : Float = event.values[1]
+            var z : Float = event.values[2]
+
+            accelX.text = "X: $x"
+            accelY.text = "Y: $y"
+            accelZ.text = "Z: $z"
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(listener)
+    }
+
+
+    /*
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {isGranted: Boolean ->
         if(isGranted) {
             println("sensor access granted")
         }
     }
+
 
     private val connectionListener = object : ConnectionListener {
         override fun onConnectionSuccess() {
@@ -87,5 +138,5 @@ class MainActivity : ComponentActivity() {
             TODO("Add error handling")
         }
     }
-
+    */
 }
