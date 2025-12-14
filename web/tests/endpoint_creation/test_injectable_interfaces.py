393a74981c75d92a5374667792ea7214ddb885ed
Noah Brandon Gagnon <noah.b.gagnon@gmail.com>
2025-12-13 19:07:03 -0500

    Changed to more appropriate type hint

--- web/tests/endpoint_creation/test_injectable_interfaces.py
+++ web/tests/endpoint_creation/test_injectable_interfaces.py
@@ -8,14 +8,14 @@ injection_test = Blueprint('injection_testing', __name__)
 @injection_route(injection_test, '/test/<user_id>', methods=["POST"])
 def user_injection_url(user: User):
     """
-    Capture user ID, inject user information into the URL, and respond with it
+    Capture user ID, inject user information into the function, and respond with it
     """
     return jsonify({"userid": user.id, "email": user.email}), 200
 
 @injection_route(injection_test, '/test/<assessment_id>/<user_id>', methods=["POST"])
 def user_and_assessment_injection_url(user: User, assessment: PatientAssessment):
     """
-    Capture user ID and assessment ID, inject user and assessment information into the URL, 
-    and respond with it
+    Capture user ID and assessment ID, inject user and assessment information into the 
+    function, and respond with it
     """
     return jsonify({"userid": user.id, "email": user.email, "assessment_id": assessment.id}), 200
