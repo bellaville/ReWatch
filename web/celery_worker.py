a5e66e05d3536375e14b3c1f29598097fb1fff54
Blindingtoast <noah.b.gagnon@gmail.com>
2026-01-06 07:17:44 -0500

    Added flask context to celery tasks

--- web/celery_worker.py
+++ web/celery_worker.py
@@ -1,25 +1,12 @@
 import os
-from celery import Celery
+from celery_app import celery
 
-# Initialize Celery application
-celery = Celery(
-    "worker",
-    broker=os.environ["REDIS_URL"],
-    backend=os.environ["REDIS_URL"],
-)
+# For discovering tasks in both the main app and test modules
+# Required to be seperated from celery_app to avoid circular imports
 
-# Add some configurations
-celery.conf.update(
-    task_ignore_result=False,
-    result_expires=3600,
-    broker_connection_retry_on_startup=True
-)
-
-# Initial Celery task discovery
 celery_routes = ["app.celery_tasks"]
 
-# Include testing tasks if in testing environment
 if os.getenv("FLASK_ENV") == "testing":
     celery_routes.append("tests.task_creation.celery_tasks")
 
-celery.autodiscover_tasks(celery_routes, related_name='', force=True)
+celery.autodiscover_tasks(celery_routes, force=True)
