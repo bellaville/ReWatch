c0e55799e0c6436303ac22c7a21bc320b8e9dd9d
clairevillan <elishaclaire80@gmail.com>
2026-01-16 11:15:10 -0500

    First iteration: new chart works by showing all reaction times and avg reaction time per assessment

--- web/app/utilities/utils.py
+++ web/app/utilities/utils.py
@@ -13,7 +13,22 @@ def get_patient_assessment_data(patient_id):
     # Create the dataset from the memory test for charts
     chart_labels = [r.date_taken.strftime("%Y-%m-%d") for r in results]
     chart_scores = [r.score for r in results]
-    chart_avg_reactions = [r.avg_reaction_time for r in results]
-    all_reaction_times = [r.reaction_times for r in results]
+    chart_avg_reactions = []
+    all_reaction_times = []
+
+    for assessment in results:
+        date_label = assessment.date_taken.strftime("%Y-%m-%d")
+        # average reactoin time (one per assessment)
+        chart_avg_reactions.append({
+            "x": date_label,
+            "y": assessment.avg_reaction_time
+        })
+
+        # Individual reaction times (many per assessment)
+        for rt in assessment.reaction_times:
+            all_reaction_times.append({
+                "x": date_label,
+                "y": rt,
+            })
 
     return results, chart_labels, chart_scores, chart_avg_reactions, all_reaction_times
