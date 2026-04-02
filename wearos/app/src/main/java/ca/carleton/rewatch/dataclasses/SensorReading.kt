a1752f96388cd9ba2ee453c3a36a5e01ba9d09a4
niklegault <nik.legault.12@gmail.com>
2026-04-01 21:36:15 -0400

    Docstrings for data classes

--- wearos/app/src/main/java/ca/carleton/rewatch/dataclasses/SensorReading.kt
+++ wearos/app/src/main/java/ca/carleton/rewatch/dataclasses/SensorReading.kt
@@ -6,6 +6,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 /**
  * Used to hold the reading from the accelerometer for a specific time.
+ *
+ * @param timestamp the timestamp for this set of data
+ * @param x the x value of the accelerometer
+ * @param y the y value of the accelerometer
+ * @param z the z value of the accelerometer
  */
 data class SensorReading(
     @SerialName("ts") val timestamp: Long,
